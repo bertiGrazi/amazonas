c186663f2e43bd5861fd937af5f5cf59fe636166
Grazielli Lima Berti <70613625+bertiGrazi@users.noreply.github.com>
2021-11-05 08:47:09 -0300

    finish react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,38 @@
 import React from 'react';
 import Product from './components/Product';
 import data from './data';
-import {  BrowserRouter } from 'react-router-dom'
-
-
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <><BrowserRouter></BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            amazona
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div>
-          <div className="row center">
-            {data.products.map(product => (
-              <Product key={product._id} product={product}></Product>
-            ))}
-          </div>
-        </div>
-      </main>
+    <>
+      <BrowserRouter>
+        <div className="grid-container">
+          <header className="row">
+            <div>
+              <a className="brand" href="/">
+                amazona
+              </a>
+            </div>
+            <div>
+              <a href="/cart">Cart</a>
+              <a href="/signin">Sign In</a>
+            </div>
+          </header>
+          <main>
+            <div>
+              <div className="row center">
+                {data.products.map(product => (
+                  <Product key={product._id} product={product}></Product>
+                ))}
+              </div>
+            </div>
+          </main>
 
-      <footer className="row center">All right reserved</footer>
-    </div></>
+          <footer className="row center">All right reserved</footer>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
